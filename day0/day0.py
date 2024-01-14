e69773eff6d6b00d33d4f4b4d563d8ab4c72a0da
likuniabbikunia <acc4you.practice@gmail.com>
2024-01-14 18:16:17 +0400

    firstcommit

---
+++ day0/day0.py
@@ -0,0 +1,50 @@
+from turtle import *
+
+speed(5)
+width(10)
+begin_fill()
+color("pink")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+end_fill()
+
+
+forward(70)
+color("sky blue")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+
+color("sky blue")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+exitonclick()
+
+
+
+
+
+
